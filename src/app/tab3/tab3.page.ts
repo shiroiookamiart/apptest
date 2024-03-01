2469443cb213403ef71f5e3b625ce77376169789
Shiro <dbeltran486@gmail.com>
2024-03-01 18:29:45 -0400

    subida cambios modulo contactos

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -12,8 +13,40 @@ export class Tab3Page {
     {"name":"Daniela", "tel": "0441-XXX-XX-XX", "par": "Prima"},
     {"name":"Oscar", "tel": "0412-XXX-XX-XX", "par": "Amigo"},
     {"name":"Lucia", "tel": "0212-XXX-XX-XX", "par": "Novia"}
-  ]
+  ] //se agrega una variable donde almacenaremos los datos
 
-  constructor() {}
+  isModalOpen = false;
 
+  contactos: FormGroup;
+
+  alertBtn = ['Aceptar'];
+  isAlertOpen = false
+
+  constructor(fb:FormBuilder){
+    this.contactos = fb.group({ //se crean los elementos para formulario
+      name: new FormControl(""),
+      tel: new FormControl(""),
+      par: new FormControl("")
+    })
+  }
+
+  createNewContact(){ //se validan y almacenan los elementos en el array
+    var name = this.contactos.value.name;
+    var tel = this.contactos.value.tel;
+    var par = this.contactos.value.par;
+    if(name != "" && tel != "" && par != ""){
+      this.arrayData.push({"name": name, "tel":tel, "par":par}); 
+      this.setOpen(false)
+    }else{
+      this.setOpenAlert(true)
+    }
+  }
+
+  setOpenAlert(isOpen: boolean) { //validacion para cerrar y abrir alert
+    this.isAlertOpen = isOpen;
+  }
+
+  setOpen(isOpen: boolean) { //validacion para cerrar y abrir formulario
+    this.isModalOpen = isOpen;
+  }
 }
